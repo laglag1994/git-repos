37503d5162756a3075d7d1a845baea34b89a057a
TajRask <roqayah1994@gmail.com>
2023-01-18 01:24:58 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Search from "./components/Search";
+import Card from "./components/Card";
 
 
 function App() {
   return (
     
-    <div className="bg-[#0D1117] bg-cover h-screen">
+    <div className="bg-[#0D1117] bg-cover h-screen ">
       <Search/>
+      <Card/>
     </div>
   );
 }
