d87304e1bb822c6d900320f17b5fec20c842fd72
TajRask <roqayah1994@gmail.com>
2023-01-18 00:41:07 +0300

    search done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,3 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
 
@@ -8,10 +7,12 @@ const Search = () => {
         <div className='bg-[#161B22] h-24 py-4 px-10 flex justify-center items-center'>
             <form>
                 <div className='flex gap-3 flex-row-reverse'>
-                    <div className='flex text-[#9CA3AF] '>
-                    <h3 className='pt-5 left-10 via-transparent text-left '>/</h3>
+
+                    <div className='flex text-[#9CA3AF] relative '>
+                    <h3 className='text-sm mt-5 left-72 px-2 via-transparent text-left absolute border-[#30363D] border-2 rounded-md'>/</h3>
                     <input className='h-8 py-2 pr-6 px-1 mt-4 w-80 bg-[#0D1117] border-[#30363D] border-2 rounded-md text-left' type="text" placeholder="Search or jump to..." />
                     </div>
+
                     <h1 className='text-6xl text-[#F0F6FC]'><FaGithub /></h1>
                 </div>
             </form>
