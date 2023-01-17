1141b3a61597ac998749415e0bdf582327c5868e
TajRask <roqayah1994@gmail.com>
2023-01-17 19:20:56 +0300

    icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,22 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+
 
 const Search = () => {
-  return (
-    <div className='bg-[#161B22] h-24 py-4 px-10 flex justify-center items-center'>
-      <form>
-        <input className='h-8 py-2 pr-6 px-1 bg-[#0D1117] border-[#30363D] border-2 rounded-md text-left' type="text" placeholder="Github Username"/>
-        <i class="fa-brands fa-github"></i>
-      </form>
-    </div>
-  )
+    return (
+        <div className='bg-[#161B22] h-24 py-4 px-10 flex justify-center items-center'>
+            <form>
+                <div className='flex gap-3 flex-row-reverse'>
+                    <div className='flex text-[#9CA3AF] '>
+                    <h3 className='pt-5 left-10 via-transparent text-left '>/</h3>
+                    <input className='h-8 py-2 pr-6 px-1 mt-4 w-80 bg-[#0D1117] border-[#30363D] border-2 rounded-md text-left' type="text" placeholder="Search or jump to..." />
+                    </div>
+                    <h1 className='text-6xl text-[#F0F6FC]'><FaGithub /></h1>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Search
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-solid-svg-icons'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
