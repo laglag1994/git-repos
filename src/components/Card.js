c7d577bf71aeb0a05e28aeca325ef978ffa6fcfe
TajRask <roqayah1994@gmail.com>
2023-01-18 01:38:10 +0300

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,15 +8,14 @@ import { FaUser } from "react-icons/fa";
 const Card = () => {
     return (
         <div className=' pt-10 flex items-center justify-center '>
-            <div className='w-[800px] h-[400px] border-[#30363D] border-2 rounded-md flex items-center justify-around '>
+            <div className='w-[700px] h-[400px] border-[#30363D] border-2 rounded-md flex items-center justify-evenly '>
 
                 <div className='flex flex-col items-center justify-center gap-5'>
                     <img src='https://avatars.githubusercontent.com/u/120031321?v=4' className="rounded-full h-[200px]" />
                     <h1 className='text-[#C9D1D9] text-3xl'>Ruqayyah</h1>
-                    <h1 className='text-[#8B949E] text-xl'>Bio</h1>
                 </div>
 
-                <div className='text-[#8B949E] text-2xl flex flex-col gap-3 justify-start items-start'>
+                <div className='text-[#8B949E] text-1xl flex flex-col gap-3 justify-start items-start'>
 
 
                     <div className='flex flex-row-reverse gap-2'>
@@ -25,14 +24,15 @@ const Card = () => {
                             <h1>Following: 2</h1>
                         </div>
 
-                        <h1 className='text-3xl text-[#8B949E] pt-5'><FaUser /></h1>
+                        <h1 className='text-2xl text-[#8B949E] pt-3'><FaUser /></h1>
                     </div>
 
                     <div className='flex flex-row-reverse gap-2'>
                     <h1>public repos: 18</h1>
-                    <h1 className='text-3xl text-[#8B949E] pt-2'><FaBook /></h1>
+                    <h1 className='text-2xl text-[#8B949E] '><FaBook /></h1>
                     </div>
 
+                    <h1 className='text-[#8B949E] text-xl pt-10'>Bio</h1>
 
                 </div>
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -9,12 +9,15 @@ const Search = () => {
                 <div className='flex gap-3 flex-row-reverse'>
 
                     <div className='flex text-[#9CA3AF] relative '>
-                    <h3 className='text-sm mt-5 left-72 px-2 via-transparent text-left absolute border-[#30363D] border-2 rounded-md'>/</h3>
+                    <button className='text-sm mt-5 left-72 px-2 via-transparent text-left absolute border-[#30363D] border-2 rounded-md'>/</button>
                     <input className='h-8 py-2 pr-6 px-1 mt-4 w-80 bg-[#0D1117] border-[#30363D] border-2 rounded-md text-left' type="text" placeholder="Search or jump to..." />
                     </div>
 
                     <h1 className='text-6xl text-[#F0F6FC]'><FaGithub /></h1>
+
+                    
                 </div>
+
             </form>
         </div>
     )
