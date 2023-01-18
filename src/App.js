5a259a698695b59e8e582fbc91ea71bca5f49d49
TajRask <roqayah1994@gmail.com>
2023-01-18 13:46:41 +0300

    api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React from "react";
 import Search from "./components/Search";
 import Card from "./components/Card";
 import Repos from "./components/Repos";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+
 
 function App() {
+
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [term, setTerm]=useState('');
+
+  useEffect(() => {
+
+    fetch(`https://api.github.com/users/${term}`)
+  }, []);
+
+
+
+
+
+
+
   return (
     
     <div className="bg-[#0D1117] bg-cover h-[100hv] ">
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -7,15 +7,15 @@ const Repos = () => {
     return (
 
 
-        <div className='pb-10'>
+        <div className='pb-10 '>
 
-            <div className='flex justify-evenly items-center gap-10 center pt-12 '>
+            <div className='flex justify-evenly items-center gap-10 center pt-12 ' >
 
                 <h1 className='text-[#58A6FF] text-3xl '>get-repos</h1>
 
 
 
-                <div className='flex flex-row-reverse gap-5'>
+                <div className='flex flex-row-reverse gap-5 '>
                     <div className='bg-[#21262D] flex gap-2 px-2 py-1 rounded-md border-[#363B42] border-2'>
 
                         <h1 className='text-[#E3B341] pt-1 '><FaStar /></h1>
@@ -25,11 +25,13 @@ const Repos = () => {
                     </div>
 
                     <div className='bg-[#1F6FEB] flex gap-2 px-8 py-2 text-white rounded-md'>
-                        <button>Visite</button>
+                        <button className='hover:opacity-80 transition-all'>Visite</button>
                     </div>
                 </div>
 
             </div>
+
+
 
 
            <div className='flex justify-evenly items-center gap-10 center pt-12 '>
